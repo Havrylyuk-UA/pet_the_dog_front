ebafa22851d2fd35746f15ec41e8b1e3a6b2dcee
Pavlo <havrylyukpavlo95@gmail.com>
2025-04-19 12:10:45 +0300

    add new npm, add base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./components/Header/Header";
 import ClickBtn from "./components/ClickBtn/ClickBtn";
 import ArmyList from "./components/ArmyList/ArmyList";
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import App from "./App.jsx";
-import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
